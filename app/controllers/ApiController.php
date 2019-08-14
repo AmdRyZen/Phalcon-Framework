8232183d03d05e64162c476fbf00274b86d5a6cf
胡志超 <huzhichao@huzhichaodeMacBook-Pro.local>
2019-08-14 14:55:04 +0800

    frist

---
+++ app/controllers/ApiController.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | API控制器
+// +----------------------------------------------------------------------
+// | Copyright (c) 2017--20xx年 捞月狗. All rights reserved.
+// +----------------------------------------------------------------------
+// | File:     api.php
+// |
+// | Author:   huzhichao@laoyuegou.com
+// | Created:  2017-xx-xx
+// +----------------------------------------------------------------------
+use Phalcon\Translate\Adapter\NativeArray;
+//use PHPExcel;
+
+class ApiController extends BaseController 
+{
+
+	public function testAction( $id = 0 ) 
+	{
+        return $this->success(['id' => $id]);
+    }
+
+}
